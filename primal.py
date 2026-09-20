adab393f0f8ab3f30137fab14e8f3e6451a113fb
CaioMendesRRosa <caiomd6@gmail.com>
2026-09-19 21:44:46 -0300

    Initial Commit

---
+++ primal.py
@@ -0,0 +1,192 @@
+import numpy as np
+from scipy import optimize
+import simplex
+
+class Primal:
+
+    def __init__ (self, A, b, Z):
+
+        self.__A = A # Restricoes
+        self.__b = b # Lado direito das restricoes 
+        self.__Z = Z # Funcao objetivo
+
+        self.__dualA = None
+        self.__dualb = None
+        self.__dualZ = None
+
+        self.__rspA = None
+        self.__rspb = None
+        self.__rspZ = None 
+
+        self.__rspColumns = None
+        self.__rspRows = None
+
+        self.__J = []
+        self.__y = []
+
+        self.__rows = len(self.__A)
+        self.__columns = len(self.__A[0])
+
+        self.__status = ""
+
+    @property
+    def status (self):
+        return self.__status
+
+    def __Setup (self):
+        # Inicializando a base
+
+        self.__dualA = np.transpose(self.__A)
+        self.__dualb = [i for i in self.__Z]
+        self.__dualZ = [i for i in self.__b]
+
+        self.__y = [0 for i in self.__b]
+
+        return
+
+
+    def __DefActiveSet (self):
+        # Encontrando o conjunto das variaveis ativas
+
+        self.__J = []
+        newJ = False
+
+        for j in range (self.__columns):
+            if self.__Z[j] - (np.array(self.__y)) @ [self.__A[i][j] for i in range(self.__rows)] == 0:
+                self.__J.append(j)
+                newJ = True
+
+        return newJ
+
+
+    def __BuildRSP (self):
+        # Construindo o problema subrestrito
+
+        self.__rspColumns = len(self.__J) + self.__rows
+        self.__rspRows = self.__rows
+
+        self.__rspZ = [0 for i in self.__J] + [1 for i in range(self.__rspRows)]
+        self.__rspb = self.__b
+        self.__rspA = []
+
+        for i in range (self.__rspRows):
+            lineI = [self.__A[i][j] for j in self.__J]
+            lineI += [0 for j in range(self.__rows)]
+            lineI[i + len(self.__J)] = 1
+            self.__rspA.append(lineI)
+
+        return
+        
+
+    def __FindMultiplier (self, simplexRSP):
+        # Encontrando o multiplicador teta
+
+        minMultiplier = np.inf
+
+        for j in range(self.__columns):
+            Aj = [self.__A[i][j] for i in range(self.__rows)]
+            mulNum = (self.__Z[j] - ( self.__y @ np.array(Aj)))
+            mulDen = (simplexRSP.dualSol[:self.__rows]) @ np.array(Aj)
+
+            if mulDen > 0 and mulNum / mulDen < minMultiplier:
+                minMultiplier = mulNum / mulDen
+
+        return minMultiplier
+
+
+    def Solver(self):
+        
+        self.__Setup()
+
+        while True:
+            newJ = self.__DefActiveSet()
+
+            if newJ == False:
+                print ("eita")
+                return
+
+            self.__BuildRSP()
+
+            simplexRSP = simplex.Simplex(self.__rspA, self.__rspb, self.__rspZ)
+            optimal, zOptimal = simplexRSP.Solver()
+
+            multiplier = self.__FindMultiplier(simplexRSP)
+
+            if multiplier != np.inf:
+                self.__y = self.__y + multiplier * np.array(simplexRSP.dualSol[:self.__rows])
+
+            optimalFound = True
+            for i in range(len(self.__J), self.__rspColumns):
+                if optimal[i] != 0:
+                    optimalFound = False
+                    break
+
+            if optimalFound:
+                xb = [0 for i in range (self.__columns)]
+                for j in range (len(self.__J)):
+                    xb[self.__J[j]] = optimal[j]
+                optimalZ = sum([self.__Z[j] * xb[j] for j in range(self.__columns)])
+                return np.array(xb), optimalZ
+            
+
+if __name__ == "__main__":
+
+    ''' 4 vertices, 5 arestas
+    A = [
+    # Aresta (s, a)
+    [-1,  1,  0,  0,  1,  0,  0,  0,  0, -1,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+    [ 1, -1,  0,  0,  1,  0,  0,  0,  0,  0, -1,  0,  0,  0,  0,  0,  0,  0,  0],
+
+    # Aresta (s, b)
+    [-1,  0,  1,  0,  0,  1,  0,  0,  0,  0,  0, -1,  0,  0,  0,  0,  0,  0,  0],
+    [ 1,  0, -1,  0,  0,  1,  0,  0,  0,  0,  0,  0, -1,  0,  0,  0,  0,  0,  0],
+
+    # Aresta (a, b)
+    [ 0, -1,  1,  0,  0,  0,  1,  0,  0,  0,  0,  0,  0, -1,  0,  0,  0,  0,  0],
+    [ 0,  1, -1,  0,  0,  0,  1,  0,  0,  0,  0,  0,  0,  0, -1,  0,  0,  0,  0],
+
+    # Aresta (a, t)
+    [ 0, -1,  0,  1,  0,  0,  0,  1,  0,  0,  0,  0,  0,  0,  0, -1,  0,  0,  0],
+    [ 0,  1,  0, -1,  0,  0,  0,  1,  0,  0,  0,  0,  0,  0,  0,  0, -1,  0,  0],
+
+    # Aresta (b, t)
+    [ 0,  0, -1,  1,  0,  0,  0,  0,  1,  0,  0,  0,  0,  0,  0,  0,  0, -1,  0],
+    [ 0,  0,  1, -1,  0,  0,  0,  0,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0, -1],
+
+    # xs = 0
+    [ 1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+
+    # xt = 1
+    [ 0,  0,  0,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0]
+    ]
+
+    b = [
+        0, 0,   # (s,a)
+        0, 0,   # (s,b)
+        0, 0,   # (a,b)
+        0, 0,   # (a,t)
+        0, 0,   # (b,t)
+        0,       # xs = 0
+        1        # xt = 1
+    ]
+
+    Z = [
+        0, 0, 0, 0,     # xs, xa, xb, xt
+        3, 4, 2, 5, 3,  # ysa, ysb, yab, yat, ybt
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0  # folgas
+    ]
+    '''
+
+    ''' 3 vertices 3 arestas
+    A = [
+        [1, 2, -1, 0],
+        [2, 1, 0, -1]
+    ]
+    b = [2, 2]
+    Z = [1, 3, 0, 0]
+    '''
+
+    primal = Primal(A, b, Z)
+    optimal, optimalZ = primal.Solver()
+    print(optimal, optimalZ)
+
---
+++ simplex.py
@@ -0,0 +1,156 @@
+import numpy as np
+from scipy import optimize
+
+class Simplex:
+
+    def __init__ (self, A, b, Z):
+
+        self.__A = A # Restricoes
+        self.__b = b # Lado direito das restricoes 
+        self.__Z = Z # Funcao objetivo
+
+        self.__B = [] # base
+        self.__Binv = [] # Inversa da Base
+        self.__Bvars = [] # variaveis da base
+        self.__dualSol = None
+
+        self.__rows = len(self.__A)
+        self.__columns = len(self.__A[0])
+
+        self.__status = ""
+
+    @property
+    def status (self):
+        return self.__status
+
+    @property
+    def dualSol (self):
+        return self.__dualSol
+
+    def __Setup (self):
+        # Inicializando a base
+        self.__Bvars = [self.__columns - self.__rows + i for i in range(self.__rows)]
+        self.__B = [[] for i in range(self.__rows)]
+
+        columnsNum = len(self.__Bvars)
+        for i in range(columnsNum):
+            for j in range(self.__rows):
+                self.__B[j].append(self.__A[j][self.__Bvars[i]])
+
+        return
+
+    def __FindPivotColumn (self, Cb, Binv):
+
+        # Fazendo Cb * Binv * A[j]
+        aux = Cb @ Binv
+        Zj = []
+        for j in range(self.__columns):
+            Aj = [self.__A[i][j] for i in range(self.__rows)]
+            Zj.append(aux @ Aj)
+
+        # Calculando os custos
+        Cj = [self.__Z[i] - Zj[i] for i in range(self.__columns)]
+
+        # Encontrando o menor custo
+        optimal = True
+        pivotColumn = -1
+        a = []
+        for i in range(self.__columns):
+            if Cj[i] < 0 and (pivotColumn != -1 or Cj[i] < Cj[pivotColumn]):
+                optimal = False
+                pivotColumn = i
+
+        if optimal:
+            self.__dualSol = aux
+
+        return optimal, pivotColumn
+
+    def __FindPivotRow (self, xb, pivotColumn):
+
+        # Fazendo invB * Aj
+        newA = self.__Binv @ [self.__A[i][pivotColumn] for i in range(self.__rows)]
+
+        # Teste da razao minima
+        bestPivotValue = np.inf
+        pivotRow = -1
+        for i in range (self.__rows):
+
+            if newA[i] < 0:
+                continue
+            
+            minTest = xb[i] / newA[i]
+            if (minTest < bestPivotValue):
+                bestPivotValue = minTest
+                pivotRow = i
+
+        return pivotRow
+
+    def Solver(self):
+
+        self.__Setup()
+
+        iterations = 0
+
+        while True:
+
+            self.__Binv = np.linalg.inv(self.__B)
+            Cb = [self.__Z[self.__Bvars[i]] for i in range (self.__rows)]
+
+            # Encontrando variavel que entra
+            optimal, pivotColumn = self.__FindPivotColumn(Cb, self.__Binv)
+
+            # Solucao atual
+            xb = self.__Binv @ self.__b
+
+            # Verificando se nao ja encontro a solucao otima
+            if optimal:
+                self.__status = f"Optimal Found in {iterations} iterations"
+
+                optimalValues = []
+                for i in range(self.__columns):
+                    if i in self.__Bvars:
+                        optimalValues.append(xb[self.__Bvars.index(i)])
+                    else:
+                        optimalValues.append(0)
+
+                zOptimal = sum([self.__Z[i] * optimalValues[i] for i in range(self.__columns)])
+                return np.array(optimalValues), zOptimal
+
+            # Encontrando variavel que sai
+            pivotRow = self.__FindPivotRow(xb, pivotColumn)
+
+            # Verificando se o problema tem solucao infinita
+            if pivotRow == -1:
+                self.__status = f"Problem has infinite solution"
+                return None
+            # Substituindo a coluna pivo
+            self.__Bvars[pivotRow] = pivotColumn
+            for i in range (self.__rows):
+                self.__B[i][pivotRow] = self.__A[i][pivotColumn]
+
+            iterations += 1
+
+if __name__ == "__main__":
+
+    A = [
+        [-1, 0, 1, 0],
+        [0, -1, 0, 1]
+        ]
+    b = [2, 2]
+    # FO
+    Z = [0, 0, 1, 1]
+
+    res = optimize.linprog(Z, A_eq=A, b_eq=b, method="highs")
+
+    # Exibindo os resultados
+    print("Solucao Scipy")
+    print(f"Valor otimo da funcao objetivo (Z): {res.fun}")
+    print(f"Solucao otima: {res.x}\n")
+
+    simplex = Simplex(A, b, Z)
+    optimal, zOptimal = simplex.Solver()
+
+    print("Solucao Simplex")
+    print (simplex.status)
+    print(f"Solucao Otima: {optimal}")
+    print(f"F* = {zOptimal}")
